9e17af852db2ddcbcf5d38deba1b6deaafffc444
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-08-02 23:54:31 +0300

    setup assets and add location model and its data service

---
+++ SwiftUIMapApp/SwiftUIMapApp/Model/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  SwiftUIMapApp
+//
+//  Created by Ahmed Amin on 02/08/2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
