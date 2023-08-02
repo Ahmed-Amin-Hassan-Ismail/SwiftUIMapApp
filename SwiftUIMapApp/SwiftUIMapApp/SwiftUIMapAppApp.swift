9d37ad74d1a2e096be165afb708a7c19c509386e
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-08-02 23:39:58 +0300

    initial commit with creating xCode project

---
+++ SwiftUIMapApp/SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Ahmed Amin on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
